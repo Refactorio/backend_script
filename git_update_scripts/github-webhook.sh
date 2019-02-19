0a1080162ceb58a8440fd72256df7bc5ed88e5d2
plague006 <plague006@gmail.com>
2019-02-19 13:36:34 -0500

    Only update on push to develop

--- git_update_scripts/github-webhook.sh
+++ git_update_scripts/github-webhook.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e #this forces an exit on any error
-#folder=$1 repository=$2 branch=$3 map=$4 cleanup=$5 debug=$6
+if [ "$1" != "develop" ]; then
+    exit 0
+fi
 
+#arguments to script: folder=$1 repository=$2 branch=$3 map=$4 cleanup=$5 debug=$6
 script_path='/home/factorio/backend_scripts/git_update_scripts/update-from-args.sh'
 log_path='/home/factorio/backend_scripts/logs/github_webhook_output.txt'
 

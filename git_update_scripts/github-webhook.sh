f464f1c568de179f585f7c89d1a1184badc69b35
plague006 <plague006@gmail.com>
2019-02-18 23:59:28 -0500

    Improve log output

--- git_update_scripts/github-webhook.sh
+++ git_update_scripts/github-webhook.sh
@@ -3,9 +3,13 @@ set -e #this forces an exit on any error
 #folder=$1 repository=$2 branch=$3 map=$4 cleanup=$5 debug=$6
 
 script_path='/home/factorio/backend_scripts/git_update_scripts/update-from-args.sh'
+log_path='/home/factorio/backend_scripts/logs/github_webhook_output.txt'
 
 {
+    echo '------------------------------------------'
+    date
     "$script_path" develop origin develop default false false
     "$script_path" crashsite origin develop crash_site false false
     "$script_path" dev-debug origin develop default false true
-} >>'/home/factorio/backend_scripts/logs/github_webhook_output.txt'
+    echo '------------------------------------------'
+} >>"$log_path"

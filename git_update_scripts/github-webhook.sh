a7a271f7437d9450613a2f32faeb8ca5d1c6e67c
plague006 <plague006@gmail.com>
2019-02-18 23:48:58 -0500

    Fix paths, use var for path

--- git_update_scripts/github-webhook.sh
+++ git_update_scripts/github-webhook.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e #this forces an exit on any error
-#log location - /home/factorio/backend_scripts/github_webhook_output.txt
 #folder=$1 repository=$2 branch=$3 map=$4 cleanup=$5 debug=$6
 
-/home/factorio/backend_scripts/update-from-args.sh develop origin develop default false false >> "/home/factorio/backend_scripts/github_webhook_output.txt"
-/home/factorio/backend_scripts/update-from-args.sh crashsite origin develop crash_site false false >> "/home/factorio/backend_scripts/github_webhook_output.txt"
-/home/factorio/backend_scripts/update-from-args.sh dev-debug origin develop default false true >> "/home/factorio/backend_scripts/github_webhook_output.txt"
+script_path='/home/factorio/backend_scripts/git_update_scripts/update-from-args.sh'
+
+{
+"$script_path" develop origin develop default false false
+"$script_path" crashsite origin develop crash_site false false
+"$script_path" dev-debug origin develop default false true
+} >> '/home/factorio/backend_scripts/logs/github_webhook_output.txt'
